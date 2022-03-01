72f6069851a664401b5678a16b7064b511844c66
masatosheep <100488578+masatosheep@users.noreply.github.com>
2022-03-01 23:40:46 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,6 @@ module RspecSample
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.i18n.default_local = :ja
+    config.i18n.default_locale = :ja
   end
 end
